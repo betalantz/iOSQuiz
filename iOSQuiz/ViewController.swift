5d46b846358df0c5877c90e69fc37c4bcb5f04c9
betalantz <betalantz@gmail.com>
2017-09-07 15:32:13 -0700

    added buttonPress actions

--- iOSQuiz/ViewController.swift
+++ iOSQuiz/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        print("Answer button pressed")
+    }
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        print("Next button pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = "How to Outlets work?"
+        answerLabel.text = "Your looking at it now!"
     }
 
     override func didReceiveMemoryWarning() {
